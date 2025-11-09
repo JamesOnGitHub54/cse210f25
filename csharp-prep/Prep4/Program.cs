50568bd80d599534cb663b3a16e4eb046bf8ccf6
JamesOnGitHub54 <jamesg521@outlook.com>
2025-11-09 00:26:17 -0700

    Core 1.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,30 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Set user number and list.
+        int user_number = -1;
+        List<int> numbers = new List<int>();
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        // Numbers will continue to be added until 0 closes loop.
+        while (user_number != 0)
+        {
+            Console.Write("Enter number: ");
+            user_number = int.Parse(Console.ReadLine());
+            if (user_number != 0)
+            {
+                numbers.Add(user_number);
+            }
+        }
+
+        // Sum of numbers.
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum = sum + number;
+        }
+        Console.WriteLine($"Sum: {sum}");
     }
 }

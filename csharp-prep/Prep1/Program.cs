7dc7d8f16567673c10c46e454885c2a1729046bb
JamesOnGitHub54 <jamesg521@outlook.com>
2025-11-08 23:36:56 -0700

    Completed Prep1.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,16 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Get the user's first and last name.
+        Console.Write("What is your first name? ");
+        string fname = Console.ReadLine();
+
+        Console.Write("What is your last name? ");
+        string lname = Console.ReadLine();
+
+        // Display the user's name as the assignment demonstrates.
+        Console.WriteLine($"\nYour name is {lname}, {fname} {lname}.");
+
     }
 }

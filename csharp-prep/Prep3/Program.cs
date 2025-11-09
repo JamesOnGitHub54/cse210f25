f7142619582143dc13c7e5791004e56404c30fd8
JamesOnGitHub54 <jamesg521@outlook.com>
2025-11-09 00:11:49 -0700

    Core requirements 1 and 2.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,29 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // User's magic number is obtained.
+        Console.Write("What is the magic number?");
+        int magic_number = int.Parse(Console.ReadLine());
+
+        // Guess is established and differs from the magic number.
+        int guess = magic_number + 1;
+
+        // Loop is followed until magic number is guessed.
+        while (guess != magic_number)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magic_number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magic_number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else Console.WriteLine("You guessed it!");
+        }
     }
 }
